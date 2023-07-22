6c8d516e5b87fec8ce2b35c5e06e1cf07231e7e6
dmrozik87 <dmrozik87@gmail.com>
2023-07-22 13:10:58 +0200

    Created SpringBoot application

---
+++ src/main/java/com/dominikmrozik/ImprovementProposalApp/ImprovementProposalApplication.java
@@ -0,0 +1,13 @@
+package com.dominikmrozik.ImprovementProposalApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImprovementProposalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImprovementProposalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dominikmrozik/ImprovementProposalApp/ImprovementProposalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dominikmrozik.ImprovementProposalApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImprovementProposalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
