c98f64b976e875618f59a3ca00edee93ae041a22
dmrozik87 <dmrozik87@gmail.com>
2023-07-22 17:00:46 +0200

    Added User and ImprovementProposal entities

---
+++ src/main/java/com/dominikmrozik/ImprovementProposalApp/entity/ImprovementProposal.java
@@ -0,0 +1,73 @@
+package com.dominikmrozik.ImprovementProposalApp.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+public class ImprovementProposal {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String status;
+    private String title;
+    private String department;
+    private String description;
+    private String review;
+    @ManyToOne(optional = false)
+    private User assignedTo;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getReview() {
+        return review;
+    }
+
+    public void setReview(String review) {
+        this.review = review;
+    }
+
+    public User getAssignedTo() {
+        return assignedTo;
+    }
+
+    public void setAssignedTo(User assignedTo) {
+        this.assignedTo = assignedTo;
+    }
+}
